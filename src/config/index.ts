b4355ec8affd5d56587e343ffd118213671c8c5c
Habibur Rahman Rahel <rahel.ahmedr@gmail.com>
2026-07-11 13:51:29 +0600

    index

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,4 +9,5 @@ export default {
     stripe_secret_key: process.env.STRIPE_SECRET_KEY as string,
     stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET as string,
     client_url: process.env.CLIENT_URL || "http://localhost:3000",
+    node_env: process.env.NODE_ENV || "development",
 };
