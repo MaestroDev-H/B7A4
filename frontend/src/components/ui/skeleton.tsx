401d96a70e631e5e5f2a86113e1901e238b7bfde
Habibur Rahman Rahel <rahel.ahmedr@gmail.com>
2026-08-01 14:49:25 +0600

    Remove frontend folder to separate repo

--- frontend/src/components/ui/skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Skeleton: React.FC<{ className?: string }> = ({ className = "" }) => {
-  return (
-    <div
-      className={`animate-pulse rounded-lg bg-zinc-200 dark:bg-zinc-800 ${className}`}
-    />
-  );
-};
